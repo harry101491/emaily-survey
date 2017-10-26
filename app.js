9073cb2ee93e2e69a1430dfb65a38447c2c3704a
Harshit Pareek <hp1014@nyu.edu>
2017-10-26 17:58:36 -0400

    feat(adding enviornment value)

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send(`Hello from Node v${process.env.NODE_ENV}`);
+    res.send(`<h2>Hello from Node ${process.env.NODE_ENV}</h2>`);
 });
 
 app.listen(PORT, () => {
