60738b9f725e8af83d706a28a87fd452faa8e9a3
Harshit Pareek <hp1014@nyu.edu>
2017-10-26 17:20:43 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send({
+        hi: "there"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`sucessfully listening on port: ${PORT}`);
+}); 
