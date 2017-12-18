75ede166e049abc33359d1948512fae6ac87eb83
Harshit Pareek <hp1014@nyu.edu>
2017-12-18 18:43:22 -0500

    feat(mail template)

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,19 @@
 module.exports = ({ body }) => {
-    return `<div> ${body} </div>`;
+    return `
+    <html>
+        <body>
+            <div style="text-align: center;">
+                <h3> I'd love to hear back from you!!</h3>
+                <p>Please! Click on one of the following links and reply the survey</p>
+                <p>${body}</p>
+                <div>
+                    <a href="http://localhost:3000">Yes</a>
+                </div>
+                <div>
+                <a href="http://localhost:3000">No</a>
+            </div>
+            </div>
+        </body>
+    </html>
+    `;
 };
