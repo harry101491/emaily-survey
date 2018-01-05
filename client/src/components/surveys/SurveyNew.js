8094612dc7a5f61c896f4538571c6fa5ca43b5cf
Harshit Pareek <hp1014@nyu.edu>
2018-01-05 01:54:18 -0500

    feat(reduxForm)

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import SurveyForm from "./SurveyForm";
 
 class SurveyNew extends Component {
     render() {
         return (
             <div>
-                <h2> SurveyNew </h2>
+                <SurveyForm />
             </div>
         );
     }
