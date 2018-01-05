e3f56844fdc995ad0b33f5a741c2834904bfcb20
Harshit Pareek <hp1014@nyu.edu>
2018-01-04 22:58:08 -0500

    feat(reduxForm)

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux";
+// importing the formReducer from the redux-form
+import { reducer as formReducer } from "redux-form";
 import authReducer from "./authReducer";
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
