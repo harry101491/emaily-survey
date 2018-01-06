67faf447c191ffa025d9cd995432164dc5a4a300
Harshit Pareek <hp1014@nyu.edu>
2018-01-06 03:32:21 -0500

    feat(surveyform)

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -5,7 +5,10 @@ class SurveyForm extends Component {
     render() {
         return (
             <div>
-                <Field type="text" name="surveyTitle" component="input" />
+                <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+                    <Field type="text" name="surveyTitle" component="input" />
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         );
     }
